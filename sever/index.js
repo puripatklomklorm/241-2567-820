9aa5ba17d2300a97549745f54246dd95a5ebd779
puripatklomklorm <puripat.klo@ku.th>
2025-02-03 17:10:25 +0700

    820

---
+++ sever/index.js
@@ -0,0 +1,16 @@
+const http = require('http'); // Import Node.js core module
+
+const host = 'localhost'; // Localhost
+
+const port = 8000; // Port number
+
+// เมื่อเปิด เว็บไปที่ http://localhost:8000/ จะเรียกใช้งาน function requireListener
+const requireListener = function (req, res) {
+    res.writeHead(200);
+    res.end('My first server!');
+}
+
+const server = http.createServer(requireListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`); // ` ` แบล้คติ้ก เอาสตรีงกับตัวแปรมาใส่ได้เลย
+});
