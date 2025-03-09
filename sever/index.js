dee00c0642bf5ff03faf6d48721bba3e332a4c08
puripatklomklorm <puripat.klo@ku.th>
2025-03-09 15:50:23 +0700

    Update index.js

--- sever/index.js
+++ sever/index.js
@@ -1,129 +1,80 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const mysql = require('mysql2/promise');
-app.use(bodyParser.json());
-const port = 8000;
+const validateData = (userData) => {
+    let errors = []
 
-let users = []
-let conn = null
+    if (!userData.firstname) {
+        errors.push('กรุณากรอกชื่อ')
+    }
+    if (!userData.lastname) {
+        errors.push('กรุณากรอกนามสกุล')
+    }
+    if (!userData.age) {
+        errors.push('กรุณากรอกอายุให้ถูกต้อง')
+    }
+    if (!userData.gender) {
+        errors.push('กรุณาเลือกเพศ')
+    }
+    if (!userData.description) {
+        errors.push('กรุณากรอกรายละเอียด')
+    }
 
-/*
-GET /users สำหรับ get ข้อมูล user ทั้งหมด
-POST /user สำหรับสร้าง create user ใหม่บันทึกเข้าไป
-PUT /user/:id สำหรับ update ข้อมูล user รายคนที่ต้องการบันทึกเข้าไป
-DELETE /user/:id สำหรับลบ user รายคนที่ต้องการออกไป
-GET /user/:id สำหรับ get ข้อมูล user รายคนที่ต้องการ
-*/
-// path = GET /users
-
-
-app.get('/testdb', (req, res) => {
-    mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password:'root',
-        database: 'webdb',
-        port: 8830
-    }).then((conn) => {
-        conn
-        .query('SELECT * FROM users')
-        .then((results) => {
-            res.json(results[0])
-        })
-        .catch((error) => {
-            console.log('Error fetching users:', error.message)
-            res.status(500).json({error: 'Error fetching users'})
-        })
-    })
- })
- 
-const initMySQL = async () => {
-  conn = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password:'root',
-      database: 'webdb',
-      port: 8830
-  })
+    return errors;
 }
 
- app.get('/testdb-new', async (req, res) => {
-  try {
-        const results = await conn.query('SELECT * FROM users')
-        res.json(results[0])
-      } catch (error) {
-        console.log('Error fetching users:', error.message)
-        res.status(500).json({error: 'Error fetching users'})
-  }
- })
+const submitData = async () => {
+    let firstnameDOM = document.querySelector('input[name=firstname]')
+    let lastnameDOM = document.querySelector('input[name=lastname]')
+    let ageDOM = document.querySelector('input[name=age]')
+    let genderDOM  = document.querySelector('input[name=gender]:checked') || {}
+    let interestDOMs = Array.from(document.querySelectorAll('input[name=interest]:checked')) ||{}
+    let descriptionDOM = document.querySelector('textarea[name=description]')
 
+    let messageDOM = document.getElementById('message')
 
-//path = / Get / Users
-app.get('/users', async (req,res) => {
-  const results = await conn.query('SELECT * FROM users')
-  res.json(results[0])
-})
+    try {
+        let interest = interestDOMs.map(el => el.value).join(',')
 
-// path = POST / User
-app.post('/users', async (req,res) => {
-    let user = req.body;
-    const results = await conn.query('INSERT INTO users SET ?', user)
-    console.log('results', results)
-    res.json({
-        message: 'User created',
-        data: results[0]
-    });
-})
-
-// path = PUT / user/:id
-app.put('/user/:id', (req,res) => {
-    let id = req.params.id;
-    let updateUser = req.body;
-    // find users from id request
-    let selectedIndex = users.findIndex(user => user.id == id)
-    // update user 
-    if (updateUser.firstname){
-        users[selectedIndex].firstname = updateUser.firstname
-    }
-    if (updateUser.lastname){
-        users[selectedIndex].lastname = updateUser.lastname
-    }
-
-    users[selectedIndex].firstname = updateUser.firstname || users[selectedIndex].firstname
-    users[selectedIndex].lastname = updateUser.lastname || users[selectedIndex].lastname
-
-    res.json({
-        message: 'User updated successfully',
-        data: {
-            user: updateUser,
-            indexUpdate: selectedIndex
+        let userData = {
+            firstname : firstnameDOM.value,
+            lastname : lastnameDOM.value,
+            age : ageDOM.value,
+            gender : genderDOM.value,
+            description : descriptionDOM.value,
+            interests : interest
         }
-    });
-    // sent user info in update to where it belongs
-    /* 
-    GET / USERS = get all users
-    POST / USERS = create new user in data
-    GET /users/:id = get user by id
-    PUT /users/:id = get user by id
-    */
- })
 
-// Path = DELETE / user/:id
-app.delete('/user/:id', (req,res) => {
-    let id = req.params.id;
-    // find index of user
-    let selectedIndex = users.findIndex(user => user.id == id)
-    
-    users.splice(selectedIndex,1)
-    delete users[selectedIndex]
-    res.json({
-        message: 'Delete Completed',
-        indexDelete: selectedIndex
-    });
-});
+        console.log('submitData', userData)
+        const errors = validateData(userData)
 
-app.listen(port, async (req,res) => {
-  await initMySQL()
-    console.log(`Server is running on port`+ port);
-});
+        if (errors.length > 0) {
+            throw {
+                message: 'กรุณากรอกข้อมูลให้ครบถ้วน',
+                errors: errors
+            }
+        }
+        
+        const response = await axios.post('http://localhost:8000/users', userData);
+        console.log('response', response.data);
+        messageDOM.innerText = "บันทึกข้อมูลเรียบร้อย";
+        messageDOM.className = "message success";
+    } catch (error) {
+        console.log('error message', error.message)
+        console.log('error', error.errors)
+        if (error.response) {
+            console.log(error.response)
+            console.message = error.response.data.message;
+            console.error = error.response.data.errors;
+        }
+
+        let htmlData = '<div>'
+        htmlData += `<div>${error.message}</div>`
+        htmlData += '<ul>'
+        for (let i = 0; i < error.errors.length; i++) {
+            htmlData += `<li>${error.errors[i]}</li>`
+        }
+        htmlData += '</ul>'
+        htmlData += '</div>'
+
+        messageDOM.innerHTML = htmlData
+        messageDOM.className = 'message danger'
+    }
+}
